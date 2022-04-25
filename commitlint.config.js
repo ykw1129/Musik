6c3619e73227c69046ab72463b1701c490d978cf
Killian <killian@8bsolutions.cn>
2022-04-25 11:09:24 +0800

    ci: 初始化配置文件

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-25 10:56:02
+ * @LastEditTime: 2022-04-25 10:56:16
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \musik\commitlint.config.js
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] }
