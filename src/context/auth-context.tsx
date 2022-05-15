eb92ca40502176effc3ea5cb18db3d6cbdedd699
ykw <1794093413@qq.com>
2022-05-16 07:21:00 +0800

    feat: 将播放器信息植入到Provider中

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,24 +1,18 @@
-import React, {  ReactNode, useReducer } from 'react';
+import React, {  Dispatch, ReactNode, useReducer } from 'react';
 import { User } from '../types';
 import { localGet } from '../utils/localStorage';
 import { Player, player } from './state';
 import { playReducer } from './reducer';
 import { Song } from '../api/resource/types';
 type ContextType = {
-    dispatch:(type:string,song?:Song)=>Player,
-    userInfo: User | undefined
-    getToken: (key:string)=> boolean|string,
-}
-const initialState = {
-    dispatch: undefined,
-    userInfo:undefined,
-    getToken:localGet('token')
-}
-export const Store = React.createContext<ContextType>(initialState);
+    state:Player
+    dispatch:Dispatch<{type:string,song?:Song}>,
+}|null
+export const Store = React.createContext<ContextType>(null);
 export const StoreProvider = ({children}:{children: ReactNode}) =>{
      const [state, dispatch] = useReducer(playReducer,player)
     return (
-        <Store.Provider value={{ dispatch}} children={children} />
+        <Store.Provider value={{dispatch,state}} children={children} />
     )
 }
 
